653035851f37307130be74966c0fae51743334f4
Rodhiyahdv <rodhiyah.desviana@gmail.com>
2021-08-26 00:33:07 +0700

    Tugas11-Pertemuan17

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ReactAsync from './components/ReactAsync';
+import HomePage from './components/HomePage';
+import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="Menu">
+        <ul>
+          <li>Home</li>
+          <li>Register</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <header>
+        <h1>Tugas 11 - Pertemuan 17</h1>
       </header>
+      <main>
+        <section id="DataApi">
+          <ReactAsync/>
+        </section>
+        <section>
+          <HomePage/>
+        </section>
+        <section>
+          <RegisterPage/>
+        </section>
+        <section>
+          <LoginPage/>
+        </section>
+      </main>
+      <footer>
+        Copyright &copy; 2021 - Rodhiyah Desviana
+      </footer>
     </div>
   );
 }
---
+++ src/components/ReactAsync.js
@@ -0,0 +1,50 @@
+import React, {useState,useEffect} from "react";
+
+const ReactAsync = () => {
+    const [pikachu, setPikachu] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://swapi.dev/api/films/?format=json");
+            const data = await response.json();
+            console.log (data.results);
+            setPikachu(data.results);
+        };
+        fetchData();
+    }, [])
+
+    return (
+        <div>
+            <h1>React Async Star Wars Films</h1>
+            <table className="TabelData">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Director</th>
+                    <th>Producer</th>
+                    <th>Release Date</th>
+                    <th>Episode</th>
+                </tr>
+                </thead>
+                <tbody>
+            
+            {pikachu.map((item,index) => (
+                <tr key={index}>
+                    <td>{item.title}</td>
+                    <td>{item.director}</td>
+                    <td>{item.producer}</td>
+                    <td style={{textAlign:'center'}}>{item.release_date}</td>
+                    <td style={{textAlign:'center'}}>{item.episode_id}</td>
+                </tr>
+                
+            ))}
+            </tbody>
+            </table>
+        </div>
+    )
+    
+
+    
+}
+
+export default ReactAsync;
